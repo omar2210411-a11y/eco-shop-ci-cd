fd8b60737f695bc262778df1d2514b5c97adccf7
t mobile <tmobile@ts-MacBook-Air.local>
2026-05-06 15:27:07 +0300

    fix products api path

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
 
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`)
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
       .then((res) => res.json())
       .then((data) => setProducts(data));
 
