6ab261d4d21ea9515978be9c6cd632deae5f9b7f
t mobile <tmobile@ts-MacBook-Air.local>
2026-05-06 16:04:24 +0300

    hardcode production api

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
 
   useEffect(() => {
 
-    fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`)
+    fetch('https://hospitable-balance-production-f76b.up.railway.app/api/products')
       .then((res) => res.json())
       .then((data) => setProducts(data));
 
